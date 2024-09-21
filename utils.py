018bbb80c831b985eb23ae6e5ea3a6f5d2a07fa7
luccacb16 <lucca.barberato@gmail.com>
2024-09-21 00:30:12 -0300

    fix: LR scheduler

--- utils.py
+++ utils.py
@@ -95,10 +95,13 @@ class ArcFaceLRScheduler(torch.optim.lr_scheduler._LRScheduler):
     def __init__(self, optimizer, reduction_epochs=None, reduction_factor=0.1, last_epoch=-1):
         self.reduction_epochs = reduction_epochs if reduction_epochs is not None else [20, 28]
         self.reduction_factor = reduction_factor
+        self.reduced = False
         super(ArcFaceLRScheduler, self).__init__(optimizer, last_epoch)
     
     def get_lr(self):
         if self.last_epoch in self.reduction_epochs:
+            self.reduced = True
+        if self.reduced:
             return [base_lr * self.reduction_factor for base_lr in self.base_lrs]
         return [base_lr for base_lr in self.base_lrs]
     
